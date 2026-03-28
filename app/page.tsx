49118cacb74fa51eccdb55737881d35ab691c8ad
cltq <shapebrabra@gmail.com>
2026-03-29 03:30:30 +0700

    fixed welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Welcome to Fumi
+            Welcome! ยินดีต้อนรับ!
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Welcome to my personal website! But unforunately, it's still a work in progress. เว็บไซต์ส่วนตัวยังอยู่ในระหว่างการพัฒนา โปรดกลับมาใหม่ในภายหลัง.
